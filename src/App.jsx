efca8ed50e444f6052b41a032b26ba731262bb42
Vincent Liussyaputra <liusvinv@gmail.com>
2023-02-13 01:58:32 +0700

    Add recipe card resul;t

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,33 @@ import DrawerExample from "./component/DrawerExample"
 import { FaSearch} from 'react-icons/fa'
 import { useEffect, useState } from "react"
 import SearchResults from "./component/SearchResults";
+import axios from "axios";
+
+async function searchMeal(query){
+  let url = new URL("https://www.themealdb.com/api/json/v1/1/search.php");
+  url.searchParams.append('s',query);
+  let response = await axios.get(url);
+  return response.data;
+   
+}
+
 function App() {
 
   const [query, setQuery] = useState('');
-  const [foodResult, setFR] = useState([1,2,3]);
+  const [searchParam,setSP] = useState(null);
+  const [foodResult, setFR] = useState(null);
   useEffect(()=>{
-    console.log(query);
+    
   },[query]);
-
+  
+  function handleSearch(){
+    if(query === "") return;
+    searchMeal(query).then((res)=>{
+      console.log(res);
+      setFR(res.meals);
+    });
+    setSP("Showing Search Result for:" + query);
+  }
   return (
     <div className="App">
       <Box as={'section'} textAlign={'center'} pt={'3em'}>
@@ -26,8 +45,9 @@ function App() {
         <Text>Welcome to TheMealDB: An open, crowd-sourced database of Recipes from around the world.</Text>
         <HStack justifyContent={'center'} margin={'2em 0'}>
           <Input placeholder="I'm looking for a recipe on how to make . . ." maxWidth={'md'} onChange={(e)=> setQuery(e.target.value)}></Input>
-          <Button leftIcon={<FaSearch/>} variant='solid' colorScheme={"teal"} minWidth={'150px'}> Search </Button>
+          <Button leftIcon={<FaSearch/>} size={'sm'} variant='solid' colorScheme={"linkedin"} minWidth={'150px'} onClick={handleSearch}> Search </Button>
         </HStack>
+        {searchParam}
         {foodResult && <SearchResults results={foodResult}></SearchResults>}
       </Box>
     </div>
--- src/component/SearchResults.jsx
+++ src/component/SearchResults.jsx
@@ -1,14 +1,20 @@
-import { Button, Card, CardBody, CardFooter, CardHeader, SimpleGrid } from "@chakra-ui/react";
+import { Button, Card, CardBody, CardFooter, CardHeader, SimpleGrid, Heading, Divider, Image,Text  } from "@chakra-ui/react";
 import { FaArrowAltCircleRight } from "react-icons/fa";
 
 export default function SearchResults({ results }) {
   return (
-    <SimpleGrid columns={3} justifyItems={'center'} mx={'auto'} gap={'2em'} maxWidth={'lg'}>
+    <SimpleGrid columns={3} my={'2'} justifyItems={'center'} mx={'auto'} gap={'2em'} maxWidth={'4xl'}>
         {results.map((a)=>{
             return (
-                <Card key={a} mx={'2em'} shadow={'md'}>
-                    <CardHeader>Recipe 1</CardHeader>
-                    <CardBody>This Recipe Contain:</CardBody>
+                <Card key={a.idMeal} mx={'2em'} shadow={'md'} outline={'2px solid black'} w={'100%'}>
+                    <CardHeader padding={'3'}>
+                        <Heading fontSize={'1rem'}textAlign={'left'}>{a.strMeal}</Heading>
+                    </CardHeader>
+                    <Divider></Divider>
+                    <CardBody padding={'0'}>
+                        <Image src={a.strMealThumb} h={'200px'} w={'100%'} objectFit={'cover'}></Image>
+                        <Text fontSize={'.8rem'}>a</Text>
+                    </CardBody>
                     <CardFooter>
                         <Button colorScheme={"linkedin"} rightIcon={<FaArrowAltCircleRight/>} variant={'solid'}> More Info </Button>
                     </CardFooter>
