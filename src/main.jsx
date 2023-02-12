3e1394d3576c034d1b9e6e90210090bc655b304c
Vincent Liussyaputra <liusvinv@gmail.com>
2023-02-12 17:07:12 +0700

    Add Chakra UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
+import { ChakraProvider, extendBaseTheme } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const theme = extendBaseTheme({
+  components:{
+
+  }
+})
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ChakraProvider theme={theme} resetCS={true}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>
 )
