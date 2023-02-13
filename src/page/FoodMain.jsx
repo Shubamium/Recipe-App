aafa399c967d0552b4331973d7c682cc0c12f1d6
Vincent Liussyaputra <liusvinv@gmail.com>
2023-02-14 02:15:00 +0700

    Add the food ingredients in the food page

--- src/page/FoodMain.jsx
+++ src/page/FoodMain.jsx
@@ -1,20 +1,43 @@
-import { Card, CardBody, SimpleGrid,Flex, CardHeader, Heading, Divider,Box, Text, Table, TableCaption, Thead, Tr, Th,Td, Tbody } from "@chakra-ui/react";
+import { Card, CardBody, SimpleGrid,Flex, CardHeader, Heading, Divider,Box, Text, Table, TableCaption,Button, Thead, Tr, Th,Td, Tbody, CardFooter } from "@chakra-ui/react";
 
 export default function FoodMain({food}) {
+  
+  function getIngredients(){
+    let res = [];
+    for(let i = 0; i < 20;i++){
+        let ingredient = food[`strIngredient${i+1}`];
+        let measure = food[`strMeasure${i+1}`];
+        if(ingredient !== ""){
+              res.push({
+                ingredient,
+                measure
+              });
+        }else{
+            break;
+        }
+    }
+    return res;
+  }
+  let ingre = getIngredients();
   return (
      <Box p={10}>
          <Flex width={'100%'} gap={'1em'} wrap={'wrap'}>
             <Card maxW={{base:'initial',xl:'30vw'}} border={'3px solid purple'} p={0} m={0}>
-                <CardHeader p2={2}>
+                <CardHeader p2={2} >
                     <Heading>
-                        Food Name
+                        {food.strMeal}
                     </Heading>
-                    <Text>Category - Area</Text>
+                    <Text>{food.strCategory} - {food.strArea}</Text>
                 </CardHeader>
                 <CardBody pt={0}>
                     Instructions:
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas doloribus ipsa laudantium eaque numquam rem, tenetur quibusdam facilis blanditiis assumenda. Iusto maxime a mollitia recusandae veniam labore omnis tempora officia!
-                </CardBody>        
+                    <p>
+                        {food.strInstructions}
+                    </p>
+                </CardBody>     
+                <CardFooter>
+                    <Button variant={'solid'} w={'100%'} mx={'0 auto'} colorScheme={'pink'}>Recipe Source!</Button>    
+                </CardFooter>   
             </Card>                
                 
             <Card flexGrow={1}  minWidth={'450px'}>
@@ -24,26 +47,30 @@ export default function FoodMain({food}) {
                 <Divider h={'2px'} color={'pink.700'}></Divider>
                 <CardBody>
                     <Box shadow={'xl'}>
-                        <iframe width="100%" height={'350px'} src="https://www.youtube.com/embed/pOJEmDBgtSk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        <iframe width="100%" height={'350px'} src="https://www.youtube.com/embed/pOJEmDBgtSk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullscreen></iframe>
                     </Box>
                 </CardBody>
             </Card>
         </Flex>
         <Divider m={5}></Divider>
         <Box borderRadius={'sm'} border={'3px solid purple'} p={5} >
-            <Table colorScheme={'purple'}>
-                <Thead>
-                    <TableCaption fontSize={'2rem'} fontWeight={'500'}>Ingredients</TableCaption>
-                    <Tr>
+            <Table colorScheme={'pink'} variant="striped" size={'lg'} >
+                <Thead >
+                    <TableCaption fontSize={'1.5rem'} w={'100%'} fontWeight={'600'}>Here's what you need!</TableCaption>
+                    <Tr bg={'pink.400'}>
                         <Th>Ingredient</Th>
-                        <Th>Measurement</Th>
+                        <Th>Measurement</Th> 
                     </Tr>
                 </Thead>
-                <Tbody>
-                    <Tr>
-                        <Td>Lentils</Td>
-                        <Td>1 Cup</Td>
-                    </Tr>
+                <Tbody fontSize={'.8rem'}>
+                    {ingre.map((recipe,index)=>{
+                        return (
+                            <Tr bg={index % 2 === 0 ? 'transparent' : 'pink.100'}>
+                                <Th>{recipe.ingredient}</Th>
+                                <Th>{recipe.measure}</Th>
+                            </Tr>
+                        )
+                    })}
                 </Tbody>
             </Table>
         </Box>
--- src/page/FoodSidebar.jsx
+++ src/page/FoodSidebar.jsx
@@ -1,14 +1,19 @@
 import { Box, Button, Heading, VStack, Image } from "@chakra-ui/react";
 import { FaArrowAltCircleLeft } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 export default function FoodSidebar({food}) {
+  let navigate = useNavigate();
   return (
    <div>
      <Box bg={"pink.900"} minHeight={'100vh'} p={0} py={5} position={'fixed'} w={'25%'}>
-        <Button variant={'solid'} leftIcon={<FaArrowAltCircleLeft/>} colorScheme={'pink'} m={4}>Go Back!</Button>
+        <Button variant={'solid'} leftIcon={<FaArrowAltCircleLeft/>} colorScheme={'pink'} m={4}
+        onClick={()=>{
+            navigate(-1);
+        }}>Go Back!</Button>
         <VStack textAlign={"left"} w={'100%'} bg={'blackAlpha.300'} my={5} borderRadius={'2em'} minH={'65vh'} maxH={'500px'}>
             {/* <Heading  color={'whiteAlpha.800'}>08530</Heading> */}
-            <Image py={10} flexGrow={1} src="https://www.themealdb.com/images/media/meals/q8sp3j1593349686.jpg" h={'300px'} w={'100%'} bg={'blackAlpha.400'} objectFit={'cover'}></Image>
+            <Image py={10} flexGrow={1} src={food.strMealThumb} h={'300px'} w={'100%'} bg={'blackAlpha.400'} objectFit={'cover'}></Image>
         </VStack>
     </Box>
    </div>
